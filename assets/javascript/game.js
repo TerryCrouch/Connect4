60e91cbb81ddaead29a3283ca1bd8085b021d19a
matlittle <matlittle727@gmail.com>
2017-10-13 08:49:41 -0500

    Logic added to track possible moves (ugly version)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,21 @@
 // Variable for grid matrix
 
-var gridMatrix = [[,,,,,],[,,,,,],[,,,,,],[,,,,,],[,,,,,],[,,,,,]]
+var gridMatrix = [[,,,,,],[,,,,,],[,,,,,],[,,,,,],[,,,,,],[,,,,,]];
 
 var ai = {
-	possMoves = [,,,,,],
-	threat = [,,,,,],
-	offense = [,,,,,]
+	possMoves: [0,0,0,0,0,0],
+	threat: [0,0,0,0,0,0],
+	offense: [0,0,0,0,0,0],
+	player: {
+		possTwo: [0,0,0,0,0,0],
+		possThree: [0,0,0,0,0,0],
+		possFour: [0,0,0,0,0,0]
+	},
+	cpu: {
+		possTwo: [0,0,0,0,0,0],
+		possThree: [0,0,0,0,0,0],
+		possFour: [0,0,0,0,0,0]
+	}
 }
 
 // Function to create the grid system and assign X/Y coordinates
@@ -54,10 +64,7 @@ function togglePlayer() {
 
 // Click event for entire grid to determine drop column
 
-
-
 // Function will add piece to the clicked column in lowest index position.  Lower left is [0] [0]
-
 function cellClicked(cell) {
 
 	var x = parseInt($(cell).attr("x"));
@@ -71,15 +78,13 @@ function cellClicked(cell) {
 	}
 
 	return;
-
 }
 
 
 // Populate piece color
-
 function addPiece(x,y) {
 	var currentCell = $(".cell[x='"+x+"'][y='"+y+"']");
-	console.log(currentCell);
+
 	$(currentCell).addClass(currentPlayer);
 	$(currentCell).removeClass("cell");
 
@@ -88,6 +93,14 @@ function addPiece(x,y) {
 	} else {
 		gridMatrix[x][y] = "b";
 	}
+
+	// update possible moves array
+	ai.possMoves[x] += 1;
+
+	determineMove();
+
+	console.log(ai.player);
+	console.log(ai.cpu);
 
 	if(winCheck(x,y)) {
 		console.log("win");
@@ -99,7 +112,6 @@ function addPiece(x,y) {
 }
 
 // Function to check if the game is won
-
 function winCheck(x, y) {
 	//console.log(x);
 	//console.log(y);
@@ -175,7 +187,6 @@ function winCheck(x, y) {
 }
 
 // If game is won, add score to proper color and reset board
-
 function updateScore() {
 	if(currentPlayer === "r") {
 		redWins += 1;
@@ -185,7 +196,6 @@ function updateScore() {
 	$("#redCount").text(redWins);
 	$("#blackCount").text(blackWins);
 }
-
 
 // Prompt if the user wins
 function promptWin() {
@@ -234,5 +244,169 @@ function resetGame() {
 	$("#pageDiv").click(function() {
 		return;
 	})
+}
 
+
+var ai = {
+	possMoves: [0,0,0,0,0,0],
+	threat: [0,0,0,0,0,0],
+	offense: [0,0,0,0,0,0],
+	player: {
+		possTwo: [0,0,0,0,0,0],
+		possThree: [0,0,0,0,0,0],
+		possFour: [0,0,0,0,0,0]
+	},
+	cpu: {
+		possTwo: [0,0,0,0,0,0],
+		possThree: [0,0,0,0,0,0],
+		possFour: [0,0,0,0,0,0]
+	}
+}
+
+// determine next move
+function determineMove() {
+
+	ai.player = {
+		possTwo: [0,0,0,0,0,0],
+		possThree: [0,0,0,0,0,0],
+		possFour: [0,0,0,0,0,0]
+	}
+	ai.cpu = {
+		possTwo: [0,0,0,0,0,0],
+		possThree: [0,0,0,0,0,0],
+		possFour: [0,0,0,0,0,0]
+	}
+
+	/* loop through possible moves array */
+	for(var i = 0; i <= 5; i += 1) {
+		/* only process if column isn't full */
+		if(ai.possMoves[i] < 6) {
+			/* get x and y coords */
+			var x = i;
+			var y = ai.possMoves[i];
+
+			var moves = { 
+				in: {
+					red: {
+						row: 0, 
+						col: 0, 
+						diagD: 0, 
+						diagU: 0
+					},
+					black: {
+						row: 0, 
+						col: 0, 
+						diagD: 0, 
+						diagU: 0
+					}
+				},
+				poss: {
+					red: {
+						row: 0, 
+						col: 0, 
+						diagD: 0, 
+						diagU: 0
+					},
+					black: {
+						row: 0, 
+						col: 0, 
+						diagD: 0, 
+						diagU: 0
+					}
+				}
+			}
+
+			ai.player = {
+				possTwo: [0,0,0,0,0,0],
+				possThree: [0,0,0,0,0,0],
+				possFour: [0,0,0,0,0,0]
+			},
+			ai.cpu = {
+				possTwo: [0,0,0,0,0,0],
+				possThree: [0,0,0,0,0,0],
+				possFour: [0,0,0,0,0,0]
+			}
+
+
+			/* row and column check */
+			for(var k = 0; k <= 5; k += 1) {
+				if(gridMatrix[x][k] === "b") {
+					moves.poss.black.col += 1;
+					moves.in.black.col += 1;
+					moves.poss.red.col = 0;
+					moves.in.red.col = 0;
+				} else if(gridMatrix[x][k] === "r"){
+					moves.poss.red.col += 1;
+					moves.in.red.col += 1;
+					moves.poss.black.col = 0;
+					moves.in.red.col = 0;
+				} else {
+					moves.poss.red.col += 1;
+					moves.poss.black.col += 1;
+				}
+
+				if(gridMatrix[k][y] === "b") {
+					moves.poss.black.row += 1;
+					moves.in.black.row += 1;
+					moves.poss.red.row = 0;
+					moves.in.red.row = 0;
+				} else if(gridMatrix[k][y] === "r"){
+					moves.poss.red.row += 1;
+					moves.in.red.row += 1;
+					moves.poss.black.row = 0;
+					moves.in.black.row = 0;
+				} else {
+					moves.poss.red.row += 1;
+					moves.poss.black.row += 1;
+				}
+
+
+				if(moves.poss.red.col === 4) {
+					if(moves.in.red.col === 2){
+						ai.player.possTwo[k] += 1;
+					} else if(moves.in.red.col === 3) {
+						ai.player.possThree[k] += 1;
+					} else if(moves.in.red.col === 4) {
+						ai.player.possFour[k] += 1;
+					}
+				}
+
+				if(moves.poss.black.col === 4) {
+					if(moves.in.black.col === 2){
+						ai.cpu.possTwo[k] += 1;
+					} else if(moves.in.black.col === 3) {
+						ai.cpu.possThree[k] += 1;
+					} else if(moves.in.black.col === 4) {
+						ai.cpu.possFour[k] += 1;
+					}
+				}
+
+				if(moves.poss.red.row === 4) {
+					if(moves.in.red.row === 2){
+						ai.player.possTwo[x] += 1;
+					} else if(moves.in.red.row === 3) {
+						ai.player.possThree[x] += 1;
+					} else if(moves.in.red.row === 4) {
+						ai.player.possFour[x] += 1;
+					}
+				}
+
+				if(moves.poss.black.row === 4) {
+					if(moves.in.black.row === 2){
+						ai.cpu.possTwo[x] += 1;
+					} else if(moves.in.black.row === 3) {
+						ai.cpu.possThree[x] += 1;
+					} else if(moves.in.black.row === 4) {
+						ai.cpu.possFour[x] += 1;
+					}
+				}
+			}
+
+
+
+
+
+		}
+
+	}
 }
